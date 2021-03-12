04c9031e1776edbdcf3a42b8caa247a7d0b36b28
zhalev <zhalev@bk.ru>
2021-03-12 15:02:28 +0300

    NGRX-4 добавлен эффект для обновления времени

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,9 @@
 import {ActionReducerMap, MetaReducer} from '@ngrx/store';
 import {environment} from '../../environments/environment';
-import {counterReducer, CounterState} from './counter';
+import {COUNTER_KEY, counterReducer, CounterState} from './counter';
 
 export interface State {
-  counter: CounterState;
+  [COUNTER_KEY]: CounterState;
 }
 
 export const reducers: ActionReducerMap<State> = {
